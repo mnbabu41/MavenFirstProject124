8cb76de397676b05db472cd6491bf685f9973771
mnbabu41 <53806791+mnbabu41@users.noreply.github.com>
2020-03-07 20:26:00 +0530

    Adding Files

---
+++ src/main/java/com/java/project/MavenFirstProject123/App.java
@@ -0,0 +1,13 @@
+package com.java.project.MavenFirstProject123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
